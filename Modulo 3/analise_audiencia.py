3cb115ad1e387592f1877ca87dae7621e3ee6470
renatosilva2022 <107969416+renatosilva2022@users.noreply.github.com>
2025-03-14 08:07:27 -0300

    Delete Modulo 3 directory

--- Modulo 3/analise_audiencia.py
+++
@@ -1,4 +0,0 @@
-# analise_audiencia.py
-def jogo_maior_audiencia(wc):
-    maior_audiencia = wc.sort_values("comparecimento", ascending=False).iloc[0]
-    return maior_audiencia
--- Modulo 3/contagem_copas.py
+++
@@ -1,7 +0,0 @@
-# contagem_copas.py
-def contar_copas(wc):
-    contagem_copas = wc["copa"].value_counts()
-    return {
-        "Masculina": contagem_copas.get("Masculina", 0),
-        "Feminina": contagem_copas.get("Feminina", 0)
-    }
